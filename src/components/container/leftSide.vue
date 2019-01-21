0512bfe62a7342efd7c8035d831c0d3607c53492
holly <holly@datastory.com.cn>
2019-01-21 10:57:31 +0800

    菜单list

--- src/components/container/leftSide.vue
+++ src/components/container/leftSide.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="left-side-box">
+    <div class="userAccount menuList">
+      <span>头像</span>
+      <span>账户名</span>
+    </div>
+    <div class="menuList addEdit"><i class='el-icon-plus'></i>新建</div>
+    <div class="menuList"><i class='el-icon-document'></i>笔记本</div>
+    <div class="menuList"><i class='el-icon-news'></i>我的分享</div>
+    <div class="menuList"><i class='el-icon-tickets'></i>分享给我的</div>
+    <div class="menuList"><i class='el-icon-delete'></i>回收 </div>
   </div>
 </template>
 
@@ -8,5 +17,5 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 </style>
