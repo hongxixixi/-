40cb2ac3f001adaa6472e269316e689d653c4f37
holly <holly@datastory.com.cn>
2019-05-30 16:26:31 +0800

    Revert "注释一些代码"
    This reverts commit 19e86c0d371c017627a8f717a1d642929c97011d.

---
+++ src/components/lists/shareToMe.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="share-to-me-lists">
+    <div class="file-lists">
+      <div
+        class="file-box"
+        v-for="(item,index) in files"
+        :key="index"
+        @click="editFile(item)"
+      >
+        <i class="iconfont icon-wenjian1"></i>
+        <div>{{item.name}}</div>
+        <div class="auth">{{item.auth.indexOf('writeAble')==-1?'可查看':'可编辑查看'}}</div>
+        <div class="auth">由用户{{item.username}}共享</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from "@/api/index.js";
+import Bus from "@/pages/common/eventBus.js";
+
+export default {
+  components: {},
+  data() {
+    return {
+      files: [],
+    };
+  },
+  computed: {
+
+  },
+  beforeDestroy() {
+
+  },
+  mounted() {
+    this.getAuthFiles();
+    Bus.$on('synch')
+  },
+  methods: {
+    //获取笔记
+    synch() {
+      alert('a')
+      this.getAuthFiles();
+    },
+    getAuthFiles() {
+      let params = JSON.stringify({ username: localStorage.username });
+      api.getAuthFiles(params).then(res => {
+        this.files = res.data.data;
+      });
+    },
+    editFile(item) {
+      let params = item;
+      params.editName = item.username;
+      params.auth = item.auth;
+      params.isBeShare = true;
+      this.$router.push({ name: "addEdit", params: { item: item } });
+    }
+  }
+};
+</script>
+
+
+
